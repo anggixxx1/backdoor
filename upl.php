78985b5c6d1c22efc36f82fb7d6c76a71f474ad1
anggixxx1 <86593615+anggixxx1@users.noreply.github.com>
2021-07-31 09:54:35 +0700

    Update upl.php

--- upl.php
+++ upl.php
@@ -14,7 +14,7 @@
 <?PHP
   if(!empty($_FILES['uploaded_file']))
   {
-    $path = "../";
+    $path = "./"; //PATH OF YOUR FILE
     $path = $path . basename( $_FILES['uploaded_file']['name']);
 
     if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
